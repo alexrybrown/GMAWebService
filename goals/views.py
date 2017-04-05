ea8f13034b92c37ebc9c9d199c8b9c6ae0223dae
alexrybrown <alexrybrown@gmail.com>
2017-04-05 17:22:53 -0600

    Added archiving url for goals

--- goals/views.py
+++ goals/views.py
@@ -23,14 +23,14 @@ class GoalViewSet(viewsets.ModelViewSet):
 
     @list_route(['get'], url_path='get-future-goals')
     def get_future_goals(self, request):
-        goals = request.user.goal_set.filter(future_goal=None).order_by('expected_completion')
+        goals = request.user.goal_set.filter(future_goal=None, archived=False).order_by('expected_completion')
         serializer = GoalSerializer(goals, many=True)
         return Response(serializer.data)
 
     @detail_route(['get'], url_path='get-sub-goals')
     def get_sub_goals(self, request, pk=None):
         goal = self.get_object()
-        sub_goals = goal.goal_set.all()
+        sub_goals = goal.goal_set.filter(archived=False)
         serializer = GoalSerializer(sub_goals, many=True)
         return Response(serializer.data)
 
@@ -43,3 +43,19 @@ class GoalViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @detail_route(['post'], url_path='archive')
+    def archive(self, request, pk=None):
+        goal = self.get_object()
+        if goal:
+            self._archive_helper(goal)
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def _archive_helper(self, goal):
+        goal.archived = True
+        goal.save()
+        if goal.goal_set.all():
+            for sub_goal in goal.goal_set.all():
+                self._archive_helper(sub_goal)
